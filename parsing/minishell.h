d135ba06bb34eade9fe8fe0da3a7cb6c69ac7517
ayoub <ayait-el@student.1337.ma>
2024-02-14 15:57:05 +0100

    added function prototypes to header file

--- parsing/minishell.h
+++ parsing/minishell.h
@@ -5,4 +5,16 @@
 
 #include "../Libft-42/libft.h"
 
+// parsing quotes
+int is_valid_token(char *token);
+char *get_next_quote(char *str);
+int count_char(char *str, char chr);
+char *remove_quotes(char *str, char quote);
+int add_new_to_list(void *content, t_list **lst);
+int add_to_list(char *edges[2], int is_quoted, char quote, t_list **lst);
+int set_start_end(int *start, int *end, char *str);
+int add_tokens_to_list(int start, int end, t_list **lst, char *str);
+int check_for_unclosed_quotes(char *command);
+t_list *handle_queotes(char *command);
+
 #endif
