dd3b47f67405577156bee3e657032087e63acefd
Mohammed Hilali <mohilali@e3r3p9.1337.ma>
2024-03-09 18:04:45 +0100

    Change the main.c

--- main.c
+++ main.c
@@ -6,68 +6,86 @@
 /*   By: mohilali <mohilali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:02:47 by mohilali          #+#    #+#             */
-/*   Updated: 2024/03/08 18:17:08 by mohilali         ###   ########.fr       */
+/*   Updated: 2024/03/09 18:04:03 by mohilali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "parsing/minishell.h"
 
+t_tree *spown_tree(t_list *lst)
+{
+	t_tree *root;
+
+	if (!lst)
+		return (NULL);
+	if (ft_open_herdocs(lst))
+		return (NULL);
+	root = build_tree(lst);
+	if (!root)
+		return (NULL);
+	return (root);
+}
+
+t_tree *parsing_check(char *promt, char **env, int *status_code)
+{
+	t_list *lst;
+	t_tree *root;
+
+	lst = split_tokens(promt);
+	if (!lst)
+		return (NULL);
+	lable_list(lst);
+	if (split_env_arg(&lst))
+		return (ft_lstclear(&lst, &free), NULL);
+	if (expand_args(&lst, env))
+		return (ft_lstclear(&lst, &free), NULL);
+	*status_code = check_syntax(lst);
+	if (*status_code != 0)
+		return (ft_lstclear(&lst, &free), NULL);
+	if (combine_list(&lst))
+		return (ft_lstclear(&lst, &free), NULL);
+	if (expand_wildcard(&lst))
+		return (ft_lstclear(&lst, &free), NULL);
+	labling_prio(lst);
+	del_spaces(&lst);
+	root = spown_tree(lst);
+	if (!root)
+		return (ft_lstclear(&lst, &free), NULL);
+	return (root);
+}
+
+int executing_part(t_tree *root, int *status_code, char **env)
+{
+	int code;
+
+    if (open_pipes(root))
+		return (0);
+	*status_code = executing_tree(root, env);
+	code = manage_pid(0, WAIT, status_code);
+	manage_fds(0, CLOSE);
+	return (0);
+}
+
 int main(int argc, char **argv, char **env)
 {
 	char *promt;
-	int code;
 	static int status_code;
-	t_list *lst;
 	t_tree *root;
 
 	(void) argc;
 	(void) argv;
-	(void) env;
 	status_code = 0;
 	while (1)
 	{
 		promt = readline("minishell %% ");
-		lst = split_tokens(promt);
-		if (lst) // TODO: handle error printing
-		{	
-			lable_list(lst);
-			if (split_env_arg(&lst))
-				return (ft_lstclear(&lst, &free), 1); // TODO: handle error
-			if (expand_args(&lst, env))
-				return (ft_lstclear(&lst, &free), 1);
-			status_code =  check_syntax(lst); // TODO: handle error
-			if (status_code)
-			{
-				printf("status == %d\n", status_code);
-				ft_lstclear(&lst, &free);
-				free(promt);
-				continue ;
-			}
-			if (combine_list(&lst))
-				return (ft_lstclear(&lst, &free), 1); // TODO: handle error
-			if (expand_wildcard(&lst))
-				return (ft_lstclear(&lst, &free), 1);
-			labling_prio(lst);
-			del_spaces(&lst);
-			if (ft_open_herdocs(lst) == 1)
-			{
-				ft_lstclear(&lst, &free);
-				continue ;
-			}
-			root = build_tree(lst);
-			if (!root)
-				return 0;
-      		if (open_pipes(root))
-	  			return (0);
-			// treeprint(root, 0);
-			status_code = executing_tree(root, env);
-			code = manage_pid(0 , WAIT, &status_code);
-			manage_fds(0, CLOSE);
-			add_history(promt);
+		root = parsing_check(promt, env, &status_code);
+		if (root)
+		{
+			status_code = executing_part(root, &status_code, env);
 			freetree(&root);
-			// ft_lstclear(&lst, &free);
 		}
+		add_history(promt);
 		free(promt);
 	}
 }
