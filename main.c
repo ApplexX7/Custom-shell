a3c85c8b979f893e758cf6e353a16fbc382fd431
Mohammed Hilali <mohilali@e3r3p3.1337.ma>
2024-03-16 22:20:16 +0000

    Up

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mohilali <mohilali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:02:47 by mohilali          #+#    #+#             */
-/*   Updated: 2024/03/16 15:21:27 by ayait-el         ###   ########.fr       */
+/*   Updated: 2024/03/16 15:41:14 by mohilali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,6 @@ t_tree *parsing_check(char *promt, char **env, int *status_code)
 		return (ft_lstclear(&lst, &free), NULL);
 	if (combine_list(&lst))
 		return (ft_lstclear(&lst, &free), NULL);
-  //print_ouput(lst);
   if (expand_args(&lst))
 		return (ft_lstclear(&lst, &free), NULL);
 	if (expand_wildcard(&lst))
