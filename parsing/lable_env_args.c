0671328979ac11925d8281514598cbb178b3ad81
Ayoub Ait El Aouad <ayait-el@e3r8p3.1337.ma>
2024-03-27 22:02:47 +0000

    fix norm

--- parsing/lable_env_args.c
+++ parsing/lable_env_args.c
@@ -1,15 +1,26 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lable_env_args.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayait-el <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 19:58:21 by ayait-el          #+#    #+#             */
+/*   Updated: 2024/03/27 19:58:38 by ayait-el         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
 
-void lable_env_args(t_list *lst)
+// else if (lst->is_op == 0 && ((char *) lst->content)[0] == '$')
+// lst->is_op = '"';
+void	lable_env_args(t_list *lst)
 {
-  while (lst)
-  {
-    if (lst->is_op != '\'' && ((char *) lst->content)[0] == '$' && ((char *) lst->content)[1] == '\0')
-      lst->is_op = '\'';
-    //else if (lst->is_op == 0 && ((char *) lst->content)[0] == '$')
-      //lst->is_op = '"';
-    lst = lst->next;
-  }
+	while (lst)
+	{
+		if (lst->is_op != '\'' && ((char *)lst->content)[0] == '$'
+			&& ((char *)lst->content)[1] == '\0')
+			lst->is_op = '\'';
+		lst = lst->next;
+	}
 }
