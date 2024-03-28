41627f40c74180d9aa27e196c67f0f233374d089
Mohammed Hilali <mohilali@e3r4p2.1337.ma>
2024-03-28 18:40:49 +0000

    Update

--- parsing/helper.c
+++ parsing/helper.c
@@ -6,7 +6,7 @@
 /*   By: mohilali <mohilali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:10:28 by mohilali          #+#    #+#             */
-/*   Updated: 2024/03/27 23:17:12 by mohilali         ###   ########.fr       */
+/*   Updated: 2024/03/28 18:40:13 by mohilali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,60 +53,6 @@ void	syntax_error_handling(t_list *copy)
 	printf("bash: syntax error in tokens\n");
 }
 
-/*
-void	lst_remove_node(t_list **lst, t_list *node)
-{
-	t_list	*tmp;
-
-	tmp = *lst;
-	if (tmp == node)
-	{
-		*lst = node->next;
-		ft_lstdelone(node, &free);
-	}
-	while (tmp->next)
-	{
-		if (tmp->next == node)
-		{
-			tmp->next = node->next;
-			ft_lstdelone(node, &free);
-			return ;
-		}
-		tmp = tmp->next;
-	}
-}
-*/
-
-/*
-int	is_space(t_list *node)
-{
-	if (!strncmp(node->content, " ", 2) && !node->is_op)
-		return (1);
-	else
-		return (0);
-}
-*/
-
-/*
-void	del_spaces(t_list **lst)
-{
-	t_list	*tmp;
-  t_list *tmp2;
-
-	tmp = *lst;
-	while (tmp)
-	{
-		tmp2 = tmp->next;
-		if (is_space(tmp))
-		{
-			lst_remove_node(lst, tmp);
-			tmp = tmp2;
-		}
-		tmp = tmp2;
-	}
-}
-*/
-
 t_list	*skip_spaces(t_list *start)
 {
 	while (start && is_space(start))
@@ -114,30 +60,11 @@ t_list	*skip_spaces(t_list *start)
 	return (start);
 }
 
-int	is_parentis(t_list *node)
-{
-	if (!ft_strncmp(node->content, "(", 2) && !node->is_op)
-		return (1);
-	if (!ft_strncmp(node->content, ")", 2) && !node->is_op)
-		return (1);
-	return (0);
-}
-
-void	del_parentis(t_list **lst)
-{
-	t_list	*tmp;
-	t_list	*tmp2;
-
-	tmp = *lst;
-	while (tmp)
-	{
-		tmp2 = tmp->next;
-		if (is_parentis(tmp))
-		{
-			lst_remove_node(lst, tmp);
-			printf("hr\n");
-			// tmp = tmp2;
-		}
-		tmp = tmp2;
-	}
-}
+// int	is_parentis(t_list *node)
+// {
+// 	if (!ft_strncmp(node->content, "(", 2) && !node->is_op)
+// 		return (1);
+// 	if (!ft_strncmp(node->content, ")", 2) && !node->is_op)
+// 		return (1);
+// 	return (0);
+// }
