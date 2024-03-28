66c34d15966c3dc00880da3a19fbe030a8f622e6
Ayoub Ait El Aouad <ayait-el@e3r1p12.1337.ma>
2024-03-28 18:20:56 +0000

    norm: env.c

--- builtins/env.c
+++ builtins/env.c
@@ -1,33 +1,41 @@
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayait-el <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 18:20:24 by ayait-el          #+#    #+#             */
+/*   Updated: 2024/03/28 18:20:33 by ayait-el         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../parsing/minishell.h"
 
-
-int ft_env(t_tree *node, t_list **local_lst)
+int	ft_env(t_tree *node, t_list **local_lst)
 {
-  static t_list **lst = NULL;
-  t_list *tmp;
-  int fd;
+	static t_list	**lst = NULL;
+	t_list			*tmp;
+	int				fd;
 
-  if (local_lst)
-    lst = local_lst;
-  else
-  {
-    if (set_fd(&fd, node))
-      return (1);
-    tmp = *lst;
-    while (tmp)
-    {
-      if (ft_strchr(tmp->content, '='))
-      {
-        ft_putstr_fd(tmp->content, fd);
-        ft_putstr_fd("\n", fd);
-      }
-      tmp = tmp->next;
-    }
-    if (fd != 1 && ft_close(fd) == -1)
-      return (perror("close"), 1);
-  }
-  return (0);
+	if (local_lst)
+		lst = local_lst;
+	else
+	{
+		if (set_fd(&fd, node))
+			return (1);
+		tmp = *lst;
+		while (tmp)
+		{
+			if (ft_strchr(tmp->content, '='))
+			{
+				ft_putstr_fd(tmp->content, fd);
+				ft_putstr_fd("\n", fd);
+			}
+			tmp = tmp->next;
+		}
+		if (fd != 1 && ft_close(fd) == -1)
+			return (perror("close"), 1);
+	}
+	return (0);
 }
